addfa7c1e393ccc69460b3e7c32a9563351618d3
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-04 16:34:53 -0300

    Add Base model to be used on all entities

--- pix/domain/model/bank.go
+++ pix/domain/model/bank.go
@@ -7,11 +7,9 @@ import (
 )
 
 type Bank struct {
-	ID        string    `json: "id"`
-	Code      string    `json: "code"`
-	Name      string    `json: "name"`
-	CreatedAt time.Time `json: "created_at"`
-	UpdatedAt time.Time `json: "updated_at"`
+	Base
+	Code string `json: "code"`
+	Name string `json: "name"`
 }
 
 func (bank *Bank) isValid() error {
