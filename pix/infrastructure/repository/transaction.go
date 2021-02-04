a2286aa010a1c2164b86259ab479b5799d253789
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-04 20:08:37 -0300

    Add orm impl of transaction repository

---
+++ pix/infrastructure/repository/transaction.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/mellomaths/pix/domain/model"
+)
+
+type TransactionRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (repository *TransactionRepositoryDb) Register(transaction *model.Transaction) error {
+	err := repository.Db.Create(transaction).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repository *TransactionRepositoryDb) Save(transaction *model.Transaction) error {
+	err := repository.Db.Save(transaction).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repository *TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
+	var transaction model.Transaction
+	repository.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("no key was found")
+	}
+	return &transaction, nil
+}
