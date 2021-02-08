cdde391153e28e2b68fdef72a84e13dc9d62791d
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-08 17:57:34 -0300

    Add complete transaction usecase

--- pix/application/usecase/transaction.go
+++ pix/application/usecase/transaction.go
@@ -50,3 +50,19 @@ func (t *TransactionUseCase) ConfirmTransaction(transactionId string) (*model.Tr
 
 	return transaction, nil
 }
+
+func (t *TransactionUseCase) CompleteTransaction(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.FindTransaction(transactionId)
+	if err != nil {
+		log.Println("Transaction not found", transactionId)
+		return nil, err
+	}
+
+	transaction.Complete()
+	err = t.TransactionRepository.SaveTransaction(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
