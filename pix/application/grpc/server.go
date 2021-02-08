afb57732f50583f0092e3c1421149b5cc867080a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-08 18:56:20 -0300

    Add pix grpc service to server

--- pix/application/grpc/server.go
+++ pix/application/grpc/server.go
@@ -6,11 +6,23 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
+	"github.com/mellomaths/pix/application/grpc/pb"
+	"github.com/mellomaths/pix/application/usecase"
+	"github.com/mellomaths/pix/infrastructure/repository"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
+
+	reflection.Register(grpcServer)
+
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
+
 	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
