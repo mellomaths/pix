ea4b8805796ef56d2876d1b54d022ed9792ba638
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-14 19:12:05 -0300

    Add kafka producer

---
+++ pix/application/kafka/producer.go
@@ -0,0 +1,31 @@
+package kafka
+
+import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+
+func NewKafkaProducer() *ckafka.Producer {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+	}
+	producer, err := ckafka.NewProducer(configMap)
+	if err != nil {
+		panic(err)
+	}
+
+	return producer
+}
+
+func Publish(msg string, topic string, producer *ckafka.Producer) error {
+	message := &ckafka.Message{
+		TopicPartition: ckafka.TopicPartition{
+			Topic:     &topic,
+			Partition: ckafka.PartitionAny,
+		},
+		Value: []byte(msg), // converts string to slice of bytes
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
