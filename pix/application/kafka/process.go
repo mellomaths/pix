b72dc683ebb4f42836e68159ee7d4c547c39f69c
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-14 20:13:26 -0300

    Add process transaction confirmation from kafka messages

--- pix/application/kafka/process.go
+++ pix/application/kafka/process.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/mellomaths/pix/application/factory"
 	applicationModel "github.com/mellomaths/pix/application/model"
+	"github.com/mellomaths/pix/application/usecase"
+	"github.com/mellomaths/pix/domain/model"
 )
 
 type KafkaProcessor struct {
@@ -49,12 +51,13 @@ func (processor *KafkaProcessor) Consume() {
 
 func (processor *KafkaProcessor) processMessage(msg *ckafka.Message) {
 	transactionsTopic := "transactions"
-	// transactionConfirmationTopic := "transaction_confirmation"
+	transactionConfirmationTopic := "transaction_confirmation"
 
 	switch topic := *msg.TopicPartition.Topic; topic {
 	case transactionsTopic:
 		processor.processTransaction(msg)
-	// case transactionConfirmationTopic:
+	case transactionConfirmationTopic:
+		processor.processTransactionConfirmation(msg)
 	default:
 		fmt.Println("not a valid topic", string(msg.Value))
 	}
@@ -85,6 +88,42 @@ func (processor *KafkaProcessor) processTransaction(msg *ckafka.Message) error {
 	transaction.ID = createdTransaction.ID
 	transaction.Status = createdTransaction.Status
 
+	transactionJson, err := transaction.ToJson()
+	if err != nil {
+		return err
+	}
+
+	err = Publish(string(transactionJson), topic, processor.Producer, processor.DeliveryChannel)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (processor *KafkaProcessor) processTransactionConfirmation(msg *ckafka.Message) error {
+	transaction := applicationModel.NewTransaction()
+	err := transaction.ParseJson(msg.Value)
+	if err != nil {
+		return err
+	}
+
+	transactionUseCase := factory.TransactionUseCaseFactory(processor.Database)
+	if transaction.Status == model.TransactionConfirmed {
+		err = processor.confirmTransaction(transaction, transactionUseCase)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (processor *KafkaProcessor) confirmTransaction(transaction *applicationModel.Transaction, transactionUseCase usecase.TransactionUseCase) error {
+	confirmedTransaction, err := transactionUseCase.ConfirmTransaction(transaction.ID)
+	if err != nil {
+		return err
+	}
+
+	topic := "bank" + confirmedTransaction.AccountFrom.Bank.Code
 	transactionJson, err := transaction.ToJson()
 	if err != nil {
 		return err
