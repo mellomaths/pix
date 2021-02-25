711c20c434c4bc209d580fcd553b42ac946a2929
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-24 21:15:17 -0300

    Add store operation to save pix keys

--- bank-api/src/controllers/pix-key/pix-key.controller.ts
+++ bank-api/src/controllers/pix-key/pix-key.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, InternalServerErrorException, Param, ParseUUIDPipe, Post, UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PixKeyDto } from 'src/dto/PixKeyDto';
+import { BankAccount } from 'src/models/bank-account';
 import { PixKey } from 'src/models/pix-key.model';
 import { Repository } from 'typeorm';
+
+import { PixService } from "src/grpc/types/pix-service.grpc";
 
 @Controller('bank-accounts/:bankAccountId/pix-keys')
 export class PixKeyController {
@@ -9,11 +14,15 @@ export class PixKeyController {
     constructor(
         @InjectRepository(PixKey)
         private pixKeyRepository: Repository<PixKey>,
+        @InjectRepository(BankAccount)
+        private bankAccountRepository: Repository<BankAccount>,
+        @Inject('PIX_PACKAGE')
+        private clientGrpc: ClientGrpc,
     ) {}
 
     @Get()
     index(
-        @Param('bankAccountId', new ParseUUIDPipe({ version: '4' })) bankAccountId: string
+        @Param('bankAccountId', new ParseUUIDPipe({ version: '4' })) bankAccountId: string,
     ) {
         return this.pixKeyRepository.find({
             where: {
@@ -26,9 +35,52 @@ export class PixKeyController {
     }
 
     @Post()
-    store() {}
+    async store(
+        @Param('bankAccountId', new ParseUUIDPipe({ version: '4' })) bankAccountId: string,
+        @Body(new ValidationPipe({ errorHttpStatusCode: 422 })) body: PixKeyDto,
+    ) {
+        await this.bankAccountRepository.findOneOrFail(bankAccountId);
+
+        const pixService: PixService = this.clientGrpc.getService('PixService');
+        
+        const notFound = await this.checkPixKeyNotFound(body);
+        if (!notFound) {
+            throw new UnprocessableEntityException("Pix Key already exists!");
+        }
+
+        const createdPixKey = await pixService.registerPixKey({
+            ...body,
+            accountId: bankAccountId,
+        }).toPromise();
+
+        if (createdPixKey.error) {
+            throw new InternalServerErrorException(createdPixKey.error);
+        }
+
+        const pixKey = this.pixKeyRepository.create({
+            id: createdPixKey.id,
+            bank_account_id: bankAccountId,
+            ...body,
+        });
+
+        return await this.pixKeyRepository.save(pixKey);
+    }
 
     @Get('exists')
     exists() {}
+
+    async checkPixKeyNotFound(params: { key: string, kind: string }) {
+        const pixService: PixService = this.clientGrpc.getService('PixService');
+        try {
+            await pixService.find(params).toPromise();
+            return false;
+        } catch (error) {
+            if (error.details === 'no key was found') {
+                return true;
+            }
+
+            throw new InternalServerErrorException("Server not available");
+        }
+    }
 
 }
